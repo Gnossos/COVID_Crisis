45f35821ead6bee4fa654b963b0f90ac920dff50
Gnossos <marsh@uri.edu>
2020-05-15 17:56:02 -0400

    Some work with inputting the data. Either load it from a saved file or download from the Internet.
    WIP

--- R/wpp2019.R
+++ R/wpp2019.R
@@ -1,30 +1,14 @@
-# get_UN_pop -- get UN population
+#' get_wpp2019 -- get UN World Population Projections - 2019 data
 
-require(data.table)
-require(ISOcodes)
-
-# United Nations Department of Economic and Social Affairs,
-#         Population Dynamics, World Population Prospects 2019.
-#         https://population.un.org/wpp/Download/Standard/CSV/
-
-# There are at least ten variations of CSV files from the 2019 UN WPP, and many, many more .XLS files.
-#   For now, this one is hard-wired. To make it easier to modify, it's moved outside the input initialization.
-un_url <-
-  "https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv"
-
-# Establish defaults.
-usa <- "United States of America"
-world <- "World"
-DEFAULT_VARIANT <- "Medium"
-
-# This comes from the data.table vignette (https://cran.r-project.org/web/packages/data.table/vignettes/datatable-intro.html)
-input <- if (file.exists("data/WPP2019_TotalPopulationBySex.csv")) {
-  "data/WPP2019_TotalPopulationBySex.csv"
-} else {
-  un_url
-}
-wpp2019 <- fread(input)
-get_wpp2019 <- function(LocID = NULL, Location = NULL, Time = NULL, File_Number = 1 ){
+get_wpp2019 <- function(LocID = NULL, Location = NULL, Time = NULL, File_Number = 1, Variant = "Medium" ){
+  #' United Nations Department of Economic and Social Affairs,
+  #'         Population Dynamics, World Population Prospects 2019.
+  #'         https://population.un.org/wpp/Download/Standard/CSV/  
+  
+  require(data.table)
+  require(ISOcodes)
+  
+  #' There are at least ten variations of CSV files from the 2019 UN WPP, and many, many more .XLS files.
   csv_files <- c(
     "WPP2019_TotalPopulationBySex",
     "WPP2019_PopulationByAgeSex_Medium",
@@ -37,7 +21,25 @@ get_wpp2019 <- function(LocID = NULL, Location = NULL, Time = NULL, File_Number 
     "WPP2019_Life_Table_Medium",
     "WPP2019_Life_Table_OtherVariants"
   )
-  file_name <- paste0("https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/",csv_files[File_Number],".csv")
+  
+  #' Construct the database name
+  UN_file_name <- paste0("data/",csv_files[File_Number])
+  
+  #' This comes from the data.table vignette (https://cran.r-project.org/web/packages/data.table/vignettes/datatable-intro.html)
+  input <- if (file.exists(UN_file_name)) {
+    UN_file_name
+  } else {
+    paste0("https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/",csv_files[File_Number],".csv")
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  wpp2019 <- fread(input)  
   
   if(is.null(LocID)){
     if (is.null(Location))
