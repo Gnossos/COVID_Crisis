1ce5a7644bf1efc5e70c3e6c6c81c0e3d67bca67
Gnossos <marsh@uri.edu>
2020-05-28 14:10:16 -0400

    Decided to develop interpolate using de Vries (2015) as the initial template. This commit begins that process, but I'm getting an error message from data.table. So I'm saving everything and restarting RStudio to follow the data.table installation instructions at https://github.com/Rdatatable/data.table/wiki/Installation.

--- R/interpolate.R
+++ R/interpolate.R
@@ -1,7 +1,25 @@
 #' interpolate
+#'
+#' See https://www.r-bloggers.com/interpolation-and-smoothing-functions-in-base-r/ (de Vries 2015) for test code & initial programming
 #' 
 
-interpolate <- function(DT,X,Y,AT,Method=smooth.spline){
+require(data.table)
+#' Everything here is for the original testing from de Vries (2015)
+set.seed(1)
+n <- 1e3
+dat <- data.table(
+  x = 1:n,
+  y = sin(seq(0,5*pi, length.out = n)) + rnorm(n=n, mean = 0, sd = 0.1)
+)
+
+approxData <- data.table(
+  with(dat,
+       approx(x, y, xout = seq(1, n, by = 10), method = "linear")
+       ),
+  method = "approx()"
+)
+
+interpolate <- function(x,y,at,method = smooth.spline){
   
   
 }
